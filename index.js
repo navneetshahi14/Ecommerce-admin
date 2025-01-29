980031396d9e2933c53480f25f58f26fff4cb8c6
navneetshahi14 <navneet.shahi2004@gmail.com>
2025-01-29 13:14:06 +0530

    few More updates

--- index.js
+++ index.js
@@ -8,24 +8,9 @@ const path = require('path')
 const cors = require('cors')
 db()
 
-const allowedOrigins = ['https://ecommercefullstack-b4bp-ad770ivj3.vercel.app/', 'https://ecommercefullstack-kk8s.vercel.app'];
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
-
-app.use(cors(corsOptions))
-app.options('*',cors())
+app.use(cors())
 app.use(bodyparser.json())
-app.use(express.json())
-app.use(express.static(path.resolve(__dirname,"build")))
-// app.use(express.static(path.resolve(__dirname,"build2")))
 
 const Authroute = require('./routes/authRoutes')
 app.use('/auth',Authroute)
@@ -39,5 +24,8 @@ app.use('/user',UserRouter)
 const payment = require('./routes/paymentroutes')
 app.use('/payment',payment)
 
+app.get('/',(req,res)=>{
+  res.send("hello world")
+})
 
 app.listen(process.env.PORT,()=>console.log(`Server Started ${process.env.PORT}`))
